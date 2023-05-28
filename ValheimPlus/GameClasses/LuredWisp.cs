47e4cd7de6f8b087818c516613e5f2282dfe50fb
Grantapher <granttpfr@gmail.com>
2023-05-28 15:54:40 -0700

    Cleaning up field references and unused imports

--- ValheimPlus/GameClasses/LuredWisp.cs
+++ ValheimPlus/GameClasses/LuredWisp.cs
@@ -1,10 +1,5 @@
 ﻿using HarmonyLib;
-using System;
-using System.Reflection;
-using System.Runtime.Remoting.Messaging;
-using UnityEngine;
 using ValheimPlus.Configurations;
-using ValheimPlus.Utility;
 using static HarmonyLib.AccessTools;
 
 namespace ValheimPlus.GameClasses
@@ -14,13 +9,13 @@ namespace ValheimPlus.GameClasses
         [HarmonyPatch(typeof(LuredWisp), "Awake")]
         public static class LuredWispPatch
         {
+            static readonly FieldRef<LuredWisp, bool> m_despawnInDaylight = FieldRefAccess<LuredWisp, bool>("m_despawnInDaylight");
 
             [HarmonyPrefix]
             static void Prefix(LuredWisp __instance)
             {
                 if (Configuration.Current.WispSpawner.IsEnabled)
                 {
-                    FieldRef<LuredWisp, bool> m_despawnInDaylight = FieldRefAccess<LuredWisp, bool>("m_despawnInDaylight");
                     m_despawnInDaylight(__instance) = Configuration.Current.WispSpawner.onlySpawnAtNight;
                 }
             }
